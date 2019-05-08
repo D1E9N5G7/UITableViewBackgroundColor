4432dfc3ac4bf1ec7f22714968e103d394e81b31
DENG <75176097@qq.com>
2019-05-08 23:56:46 +0800

    AccessoryType

--- UITableViewBackgroundColor/ViewController.swift
+++ UITableViewBackgroundColor/ViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate{
+    
+    var mons = ["january","feberuary","march","april","may"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let mUITableView = UITableView(frame: self.view.bounds)
+        mUITableView.delegate = self
+        mUITableView.dataSource = self
+        self.view.addSubview(mUITableView)
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let reusedId = "reuse"
+        var cell = tableView.dequeueReusableCell(withIdentifier: reusedId)
+        if(cell == nil){
+            cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: reusedId)
+        }
+        let index = (indexPath as NSIndexPath).row
+        
+        cell?.textLabel?.text = mons[index]
+        
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if(cell?.accessoryType == UITableViewCell.AccessoryType.none){
+            cell?.accessoryType = UITableViewCell.AccessoryType.checkmark
+        }else{
+            cell?.accessoryType = UITableViewCell.AccessoryType.none
+        }
+    }
 
 }
 
